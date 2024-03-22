cd9d6c060460dd37e9ece595ce4b4ec0fefedf0f
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-03-23 03:44:29 +1100

    Updated AboutMe section

--- src/features/aboutme/AboutMe.tsx
+++ src/features/aboutme/AboutMe.tsx
@@ -11,10 +11,15 @@ const AboutMe: FC<object> = () => {
             sectionRef.current.scrollIntoView({ behavior: "smooth" });
         }
     }
-    return <section className="h-screen" ref={sectionRef}>
-        <h1>
-            Hello world
-        </h1>
+    return <section className="h-screen grid place-items-center text-xl grid-cols-2 grid-rows-1" ref={sectionRef}>
+        <p className="text-left max-w-96 leading-loose mb-4">
+            I'm a graduate from the University of Melbourne, passionate about technology and currently
+            working as a full stack developer in Australia.
+            <div className="h-4"/>
+            In my own time, I'm a digitial artist and I like to sometimes experiment with music.
+        </p>
+        <img className="w-96" src="brs1.png"/>
+        
     </section>
 }
 
--- src/features/intro/Intro.tsx
+++ src/features/intro/Intro.tsx
@@ -9,7 +9,7 @@ const Intro: FC<object> = () => {
         dispatch(updateScrollContext("AboutMe"));
     };
 
-    return <section className="bg-backdrop h-screen bg-cover grid place-items-center">
+    return <section className="bg-backdrop h-screen bg-cover grid place-items-center border-black border-[16px]">
         <div className="absolute h-64 bg-gradient-to-t from-black to-transparent w-full bottom-0 left-0 cursor-pointer" onClick={toNextSection}/>
         <svg className="absolute translate-x-[-50%] bottom-0 left-1/2 animate-bounce pointer-events-none" width="48" height="48" viewBox="0 0 24 24" fill="white"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
         
