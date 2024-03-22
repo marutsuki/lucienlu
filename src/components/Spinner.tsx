12cf5bb997d5f396c07548a8235a54ef778d4527
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-03-22 23:55:40 +1100

    Updates to spinner aesthetics

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 
+const spinnerCss = "absolute animate-overshootSpin w-48"
 const Spinner: FC<object> = () => {
-    return <span>
-        <img className="absolute animate-overshootSpin w-48 animate-delay-0" src="/spinner.png"/>
-        <img className="absolute animate-overshootSpin w-48 animate-delay-[50ms] opacity-80" src="/spinner.png"/>
-        <img className="absolute animate-overshootSpin w-48 animate-delay-[100ms] opacity-60" src="/spinner.png"/>
-        <img className="absolute animate-overshootSpin w-48 animate-delay-[150ms] opacity-40" src="/spinner.png"/>
-        <img className="absolute animate-overshootSpin w-48 animate-delay-[200ms] opacity-20" src="/spinner.png"/>
+    return <span className="block w-48 h-48">
+        <img className={spinnerCss.concat(" animate-delay-0 blur-md")} src="/spinner.png"/>
+        <img className={spinnerCss.concat(" animate-delay-0")}  src="/spinner.png"/>
+        <img className={spinnerCss.concat(" animate-delay-[50ms] opacity-80")} src="/spinner.png"/>
+        <img className={spinnerCss.concat(" animate-delay-[100ms] opacity-60")} src="/spinner.png"/>
+        <img className={spinnerCss.concat(" animate-delay-[150ms] opacity-40")} src="/spinner.png"/>
+        <img className={spinnerCss.concat(" animate-delay-[200ms] opacity-20")} src="/spinner.png"/>
     </span>
 }
 
